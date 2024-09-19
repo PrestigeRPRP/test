54150f78d43a523f0e678edb194aed2350fbdaa1
PrestigeRPRP <sf3149422@gmail.com>
2024-09-20 07:02:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+/*
+  AFRAME.registerComponent('music-2-handler', {
+    init: function () {
+      let playing = false;
+      let audio = document.querySelector("#music2");
+      audio.volume = 0.06; // Set the volume to 0.06
+      this.el.addEventListener('click', () => {
+        if (!playing) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+        playing = !playing;
+      });
+    }
+  })
+
+
+  AFRAME.registerComponent('music-3-handler', {
+    init: function () {
+      let playing = false;
+      let audio = document.querySelector("#music3");
+      this.el.addEventListener('click', () => {
+        if (!playing) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+        playing = !playing;
+      });
+    }
+  })
+
+  */
